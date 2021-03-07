a411084b334743f1daa45a4dedec679b1d802833
Rei Okumura <19339328+kamuiroeru@users.noreply.github.com>
2021-03-07 14:26:07 +0900

    時間表示に対応

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 # 定期実行用スクリプト
+
+cd $(dirname $0)  # このスクリプトのディレクトリに移動
+
 source .venv/bin/activate
 
+# 80文字数の線を書く
+echo -n "# "
+for _ in `seq 1 39`; do
+    echo -n "--"  # "-" だと表示されなかった
+done
+echo 
+date
+
+# 読み込む
 python update_sega_log.py
 python notify.py
